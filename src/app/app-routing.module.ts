f05df470f782d2e8682b67e2a026083590a6d9e6
joshuatrefzer <121818554+joshuatrefzer@users.noreply.github.com>
2025-03-22 13:47:18 +0100

    chore: removed unused imports

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LoginComponent } from './login/login.component';
-import { SignUpComponent } from './sign-up/sign-up.component';
-import { StartscreenComponent } from './startscreen/startscreen.component';
-import { TemplateComponent } from './template/template.component';
 import { SummaryComponent } from './summary/summary.component';
 import { AddtaskComponent } from './addtask/addtask.component';
 import { LegalsComponent } from './legals/legals.component';
 import { ContactsComponent } from './contacts/contacts.component';
 import { BoardComponent } from './board/board.component';
-import { AppComponent } from './app.component';
 import { AboutComponent } from './about/about.component';
 
 
